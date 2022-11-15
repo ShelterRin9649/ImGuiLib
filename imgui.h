john0693/imgui

ImStrv: added imconfig class extension example, added natvis description.

imgui.h
@@ -250,7 +250,8 @@ struct ImVec4
 #endif
 };
 
-// String view class.
+// String view (non-owning pair of begin/end pointers, not necessarily zero-terminated)
+// ImStrv are used as function parameters instead of passing a pair of const char*.
 #define IMGUI_HAS_IMSTR 1
 struct ImStrv
 {
@@ -261,9 +262,9 @@ struct ImStrv
     ImStrv(const char* b, const char* e){ Begin = b; End = e ? e : b + strlen(b); }
     inline size_t length() const        { return (size_t)(End - Begin); }
     inline bool empty() const           { return Begin == End; }    // == "" or == NULL
-    inline operator bool() const        { return Begin != NULL; }   // != NULL
-#ifdef IM_IMSTR_CLASS_EXTRA
-    IM_IMSTR_CLASS_EXTRA     // Define additional constructors and implicit cast operators in imconfig.h to convert back and forth between your string types and ImStrv.
+    inline operator bool() const        { return Begin != NULL; }   // return true when valid ("" is valid, NULL construction is not)
+#ifdef IM_STR_CLASS_EXTRA
+    IM_STR_CLASS_EXTRA     // Define additional constructor in imconfig.h to convert your string types (e.g. std::string, std::string_view) to ImStrV.
 #endif
     // private: bool operator==(ImStrv) { return false; } // [DEBUG] Uncomment to catch undesirable uses of operators
     // private: bool operator!=(ImStrv) { return false; }

ShelterRin9649/ImGuiLib

imgui.h
@@ -273,7 +273,8 @@ struct ImVec4
 #endif
 };
 
-// String view class.
+// String view (non-owning pair of begin/end pointers, not necessarily zero-terminated)
+// ImStrv are used as function parameters instead of passing a pair of const char*.
 #define IMGUI_HAS_IMSTR
 struct ImStrv
 {
@@ -284,9 +285,9 @@ struct ImStrv
     ImStrv(const char* b, const char* e){ Begin = b; End = e ? e : b + strlen(b); }
     inline size_t length() const        { return (size_t)(End - Begin); }
     inline bool empty() const           { return Begin == End; }    // == "" or == NULL
-    inline operator bool() const        { return Begin != NULL; }   // != NULL
-#ifdef IM_IMSTR_CLASS_EXTRA
-    IM_IMSTR_CLASS_EXTRA     // Define additional constructors and implicit cast operators in imconfig.h to convert back and forth between your string types and ImStrv.
+    inline operator bool() const        { return Begin != NULL; }   // return true when valid ("" is valid, NULL construction is not)
+#ifdef IM_STRC_CLASS_EXTRA
+    IM_STRV_CLASS_EXTRA     // Define additional constructor in imconfig.h to convert your string types (e.g. std::string, std::string_view) to ImStrV.
 #endif
     // private: bool operator==(ImStrv) { return false; } // [DEBUG] Uncomment to catch undesirable uses of operators
     // private: bool operator!=(ImStrv) { return false; }